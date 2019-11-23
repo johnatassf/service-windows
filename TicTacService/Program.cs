3b948af08a78954f01915a0f250c30c67fbeed3d
Johnatas Santos <johnatas.sf@gmail.com>
2019-11-23 18:01:15 -0300

    Criado projeto 'Console Application' e utilizado TopShelf para transformar em Windows Service

---
+++ TicTacService/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TicTacService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(config =>
+            {
+                config.Service<TicTac>(service =>
+               {
+                   service.ConstructUsing(tictac => new TicTac());
+                   service.WhenStarted(tictac => tictac.Start());
+                   service.WhenStopped(tictac => tictac.Stop());
+               });
+
+                config.RunAsLocalSystem();
+
+                config.SetServiceName("TicTacService");
+                config.SetDisplayName("TicTac Service");
+                config.SetDescription("This is sample tictac service for let's working with a Services Windows with a TopShelf");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetType());
+            Environment.ExitCode = exitCodeValue;
+
+        }
+    }
+}
---
+++ TicTacService/TicTac.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TicTacService
+{
+    public class TicTac
+    {
+        private readonly Timer _timer;
+
+        public TicTac()
+        {
+            _timer = new Timer(1000 * 1) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\Users\T2S-JO~1\AppData\Local\Temp\Demos\TicTac.txt", lines);
+        }
+        // Method configured in class Program when service is Stated
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+
+        // Method configured in class Program when service is Stopped
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
